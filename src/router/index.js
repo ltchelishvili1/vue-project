673969b30237c4142054b522ff7b427f9d95f937
ltchelishvili1 <levan.tchelishvili@redberry.ge>
2023-05-07 11:10:50 +0400

    test2

--- src/router/index.js
+++ src/router/index.js
@@ -26,13 +26,14 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/test22',
-      name: 'test22',
+      path: '/test2',
+      name: 'test2',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    
 
   ]
 })
